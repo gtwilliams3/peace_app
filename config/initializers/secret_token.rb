cf04dbae7cfe6934df5433eb737cedac2c9d5e2f
Geoffrey Williams <gtwilliams1@gmail.com>
2011-04-24 18:01:59 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PeaceApp::Application.config.secret_token = '41830a40a02babbf3e43175cbcaeb17668fe381f9367effdf1d34069c8120d62f735de124a303148927505bcd1753165c161befb606a9e1dc609011b68dfc1eb'
